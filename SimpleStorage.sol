9fba3c3c3cb18322e9173c9c73fa8a4e08d5090a
Roby <42898649+xor68@users.noreply.github.com>
2022-09-06 21:28:08 +0200

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,19 +1,35 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
 
 contract SimpleStorage {
-  struct People {
-    uint256 favoriteNumber;
-    string name;
-  }
-  
-  People[] public people;
-  
-  mapping(string => uint256) public nameToFavoriteNumber;
-  
-  function addPerson(string memory _name, uint256 _favoriteNumber) public {
-    people.push(People(_favoriteNumber, _name));
-    nameToFavoriteNumber[_name] = _favoriteNumber;
-  }
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+
+    mapping(string => uint256) nameToFavoriteNumber;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    //mapping(string => uint256) public nameToFavoriteNumber;
+    function PersonToNumber (string memory _name) public view returns (uint256) {
+        return nameToFavoriteNumber[_name];
+    }
+
+    function NumberToPerson(uint256 _num) public view returns (string memory) {
+        return people[_num].name;
+    }
+
+    function dimension () public view returns (uint256) {
+        return people.length;
+    }
+
 }
